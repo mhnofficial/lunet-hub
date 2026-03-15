e80bc3d1f43a03a2e6fdf3ef59cb98a9bf5060f5
mhnofficial <nickygomez.29@icloud.com>
2026-03-15 00:40:08 -0400

    debug sw fetch

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 importScripts("/scram/scramjet.all.js");
 
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
+const scramjet = new ScramjetServiceWorker();
 
 self.addEventListener("install", (event) => {
     event.waitUntil(self.skipWaiting());
@@ -10,16 +11,10 @@ self.addEventListener("activate", (event) => {
     event.waitUntil(clients.claim());
 });
 
-let scramjet;
-
-self.addEventListener("message", (event) => {
-    if (event.data?.type === "init") {
-        scramjet = new ScramjetServiceWorker(event.data.config);
-    }
-});
-
 self.addEventListener("fetch", (event) => {
-    if (scramjet?.route(event)) {
+    console.log("SW fetch:", event.request.url);
+    if (scramjet.route(event)) {
+        console.log("SW routing:", event.request.url);
         event.respondWith(scramjet.fetch(event));
     }
 });
