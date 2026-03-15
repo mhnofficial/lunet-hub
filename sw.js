fa2edf9045e9dca15a12372f5d1b9e050230af37
mhnofficial <nickygomez.29@icloud.com>
2026-03-14 20:25:56 -0400

    add bare server

--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
 importScripts("/scram/scramjet.all.js");
+
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
+
+scramjet.config.bare = ["https://baremux.torahanytime.com/"];
+
 self.addEventListener("fetch", (event) => {
     if (scramjet.route(event)) {
         event.respondWith(scramjet.fetch(event));
