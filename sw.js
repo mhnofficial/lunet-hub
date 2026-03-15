005c5a3ee944558d85ab84adf04f617f75e7ad69
mhnofficial <nickygomez.29@icloud.com>
2026-03-15 00:10:25 -0400

    sw skipWaiting and claim clients

--- sw.js
+++ sw.js
@@ -3,6 +3,14 @@ importScripts("/scram/scramjet.all.js");
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 
+self.addEventListener("install", (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(clients.claim());
+});
+
 self.addEventListener("fetch", (event) => {
     if (scramjet.route(event)) {
         event.respondWith(scramjet.fetch(event));
