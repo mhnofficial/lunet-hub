6b5cd951706e685d957b118561f4d59f23bcbd88
mhnofficial <nickygomez.29@icloud.com>
2026-03-14 20:23:47 -0400

    fix proxy routing

--- sw.js
+++ sw.js
@@ -1,8 +1,6 @@
 importScripts("/scram/scramjet.all.js");
-
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
-
 self.addEventListener("fetch", (event) => {
     if (scramjet.route(event)) {
         event.respondWith(scramjet.fetch(event));
